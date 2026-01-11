1f19819b284f1043317fae2f76426d31a3b65dcb
Jallepalli Sai Nikhil <115182270+Jallepalli-Sai-Nikhil@users.noreply.github.com>
2026-01-11 22:22:25 +0530

    Update quotes.js

--- quote-cards/src/data/quotes.js
+++ quote-cards/src/data/quotes.js
@@ -1,11 +1,24 @@
 // data/quotes.js
 const quotes = [
-  { text: "Breathe, the world can wait.", category: "Peaceful White" },
-  { text: "Stillness holds its own power.", category: "Peaceful White" },
-  { text: "Kindness changes everything quietly.", category: "Green Good" },
-  { text: "Grow where you are planted.", category: "Green Good" },
-  { text: "Boldness conquers fear.", category: "Bold Red" },
-  
+  {
+    text: "Empathy without boundaries is self-destruction", 
+    category: "Bold Red"
+  },
+  {
+    text: "Loose lips sink big ships.",
+
+    // "You need to tear muscle to grow it. why would it be differnt for the mind or spirit."
+    // "Saying someone is ugly doesn't make you any prettier"
+    // If you really want to do something, you’ll find a way. If you don’t, you’ll find an excuse.
+    // Never do something permanently foolish just because you are temporarily upset.
+    // In life, if you don’t risk anything, you risk everything.
+    // Trying to be someone else is a waste of the person you are.
+    // When someone tells you, “You’ve changed,” it might simply be because you’ve stopped living your life their way.
+    // Someone else doesn’t have to be wrong for you to be right.
+    // only a fool trips on what is behind
+    // The river doesn't argue with the rokcs, it flows around them adopt its wisdom
+    // 
+  }
 ];
 
 export default quotes;
